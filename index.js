8aed0d662e1efc5cdce64652aa8f7e639228d274
Saad Ahmed <saadahmedcronos@gmail.com>
2024-09-14 21:55:57 +0500

    feat: add dark mode

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 document.querySelector('.toggle').addEventListener('click', () => {
   document.querySelector('.menu').classList.toggle('active');
 });
+
+// const theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+//   document.documentElement.setAttribute('data-theme', theme);
+
+//   window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+//     const newTheme = e.matches ? 'dark' : 'light';
+//     document.documentElement.setAttribute('data-theme', newTheme);
+//   });
+
